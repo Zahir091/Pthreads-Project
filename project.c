47b16df497954f6441519914b1718997e73368f2
Zahir091 <176171770+Zahir091@users.noreply.github.com>
2025-10-31 17:18:08 -0400

    Added two more algorithms

--- project.c
+++ project.c
@@ -41,7 +41,6 @@ void readFile(const char *filename) {
 }
 
 int main() {
-    srand(time(NULL)); //  random number generation using time for complete randomness
     readFile("tasks.txt"); //Reading for our input file
 
     //Declaring and creating the threads for each scheduling algorithm
@@ -64,7 +63,7 @@ int main() {
 void *fcfs(void *param) {
     int waitingTime[5]; //Holds waiting times for each task
     int turnaroundTime[5]; //Holds turnaround times for each task
-    int totalWaiting = 0, TotalTurnaroundTime = 0;
+    int totalWaiting = 0, TotalTurnaroundTime = 0; //The total overall from the tasks
 
     waitingTime[0] = 0; //Waiting time for first task is 0
     
@@ -81,6 +80,7 @@ void *fcfs(void *param) {
     //Printing the Gnatt Chard
     printf("\n--- FCFS Scheduling ---\n");
     printf("Gnatt Chart: ");
+
     for(int i=0; i < numOfTasks; i++) {
         printf("[T%d]", tasks[i].taskID); //Printing for each task
     }
@@ -88,15 +88,159 @@ void *fcfs(void *param) {
 
     //Loop through tasks again and find Total Waiting and Turnaround times
     for(int i=0; i<numOfTasks; i++) {
-        printf("Task %d: Waiting = %d, Turnaround = %d\n", 
-            tasks[i].taskID, waitingTime[i], turnaroundTime[i]);
+        printf("Task %d: Waiting = %d, Turnaround = %d\n", tasks[i].taskID, waitingTime[i], turnaroundTime[i]);
         totalWaiting += waitingTime[i]; //Adding the wait times into total
         TotalTurnaroundTime += turnaroundTime[i]; //Adding the turnaround times into total
     }
 
-     //Calculating average waiting time/turnaround time and rounding
+    //Calculating average waiting time/turnaround time and rounding
     printf("Average Waiting Time: %.2f\n", (float)totalWaiting/numOfTasks);
     printf("Average Turnaround Time: %.2f\n", (float)totalTurnaround/numOfTasks);
     pthread_exit(0);
 }
 
+void *sjf(void *param) {
+    int waitingTime[5]; 
+    int turnaroundTime[5]; 
+    int totalWaiting = 0, TotalTurnaroundTime = 0;
+
+    //Array to hold sorted tasks needed for SJF Scheduling 
+    struct Task sortedTasks[5]; 
+    for(int i=0; i<numOfTasks; i++) {
+        sortedTasks[i] = tasks[i]; //Copying original tasks into sorted array 
+    }
+
+    //Sorting the tasks in the sorted array based on burst time using Bubble Sorting
+    for (int i=0; i<numOfTasks-1; i++) {
+        for(int j=0; j<numOfTasks-i-1; j++) {
+            if(sortedTasks[j].burstTime > sortedTasks[j+1].burstTime) {
+                struct Task temp = sortedTasks[j];
+                sortedTasks[j] = sortedTasks[j+1];
+                sortedTasks[j+1] = temp;
+            }
+        }
+    }
+
+    waitingTime[0] = 0; //Waiting time for first task is 0
+
+    //Caculating the waiting time for each task after the sorting has happened
+    for(int i=1; i < numOfTasks; i++) {
+        waitingTime[i] = sortedTasks[i-1].burstTime + waitingTime[i-1];
+    }
+
+    //Calculating turnaround time for each task after the sort
+    for(int i=0; i < numOfTasks; i++) {
+        turnaroundTime[i] = sortedTasks[i].burstTime + waitingTime[i];
+    }
+
+    //Printing the Gnatt Chart
+    printf("\n--- FCFS Scheduling ---\n");
+    printf("Gnatt Chart: ");
+    for(int i=0; i < numOfTasks; i++) {
+        printf("[T%d]", sortedTasks[i].taskID); //Printing for each task
+    }
+    printf("\n");
+
+    //Printing the waiting and turnaroundTime
+    for(int i=0; i<numOfTasks; i++) {
+        printf("Task %d: Waiting = %d, Turnaround = %d\n", sortedTasks[i].taskID, waitingTime[i], turnaroundTime[i]);
+        totalWaiting += waitingTime[i]; //Adding the wait times into total
+        TotalTurnaroundTime += turnaroundTime[i]; //Adding the turnaround times into total
+    }
+
+    //Calculating average waiting time/turnaround time and rounding
+    printf("Average Waiting Time: %.2f\n", (float)totalWaiting/numOfTasks);
+    printf("Average Turnaround Time: %.2f\n", (float)TotalTurnaroundTime/numOfTasks);
+
+    pthread_exit(0);
+}
+
+void *priority_Scheduling(void *param) {
+
+    //Storing wait times and turnaround times for each task 
+    int waitingTime[5] = {0};
+    int turnaroundTime[5] = {0};
+
+    //Used to track how much burst time is left for each task
+    int remainingBurst[5];
+
+    //Checks if the task is completed its run
+    int isCompleted[5] = {0};
+
+    int totalWaiting = 0, TotalTurnaroundTime = 0;
+    int completedTasks = 0;
+    int currentTime = 0; 
+
+    srand(time(NULL)); // Makes sure random numbers are random each time
+
+    for(int i=0; i<numOfTasks; i++) {
+        tasks[i].arrivalTime = rand() % 101; // Random arrival time assignments between 0 and 100 ms
+        remainingBurst[i] = tasks[i].burstTime; // Copying the bursts times for computations
+    }
+
+
+    //Printing the generated arrival times
+    printf("Generated Arrival Times: \n");
+    for (int i=0; i<numOfTasks; i++) {
+        printf("Task %d: Arrival Time = %d ms, Priority: %d, Burst: %d\n", tasks[i].taskID, tasks[i].arrivalTime, tasks[i].priority, tasks[i].burstTime);
+    }
+
+    printf("\n--- Priority Scheduling ---\n");
+    printf("Gnatt Chart: ");
+
+
+    //Running till all tasks are completed
+    while(completedTasks < numOfTasks) {
+        int current = -1; //The task we are on
+        int highestPriority = 100; //Used to track which task has highest priority, lower number = highest 
+
+        //Finding the task with the highest priority that has arrived
+        for(int i=0; i<numOfTasks; i++) {
+            if(tasks[i].arrivalTime <= currentTime && !isCompleted[i]) {
+                //if the priorrity is higher than our current highest priority
+                if(tasks[i].priority < highestPriority) {
+                    highestPriority = tasks[i].priority; //make this our new highest priority
+                    current = i; //current task running
+                }
+            }
+        }
+
+        /
+        //If we have a current task to execute
+        if(current != -1) {
+            //Executing the selected task
+            printf("[T%d]", tasks[current].taskID);
+            //Executing task for one time unit 
+            remainingBurst[current]--; 
+            currentTime++;
+
+            //If task is finished
+            if(remainingBurst[current]==0) {
+                isCompleted[current] = 1; //completed
+                completedTasks++;
+                int finishTime = currentTime; //storing finish time
+
+                //calculations
+                turnaroundTime[current] = finishTime - tasks[current].arrivalTime;
+                waitingTime[current] = turnaroundTime[current] - tasks[current].burstTime;
+                totalWaiting += waitingTime[current];
+                TotalTurnaroundTime += turnaroundTime[current];
+            }
+        //If no current task to execute go on
+        }  else {
+                currentTime++; //If no task is ready, increment time
+            }
+    }
+
+    printf("\n");
+
+    //Printing the waiting and turnaroundTime for each task
+    for(int i=0; i<numOfTasks; i++) {
+        printf("Task %d: Waiting = %d, Turnaround = %d\n", tasks[i].taskID, waitingTime[i], turnaroundTime[i]);
+    }
+
+    printf("Average Waiting Time: %.2f\n", (float)totalWaiting/numOfTasks);
+    printf("Average Turnaround Time: %.2f\n", (float)TotalTurnaroundTime/numOfTasks);
+
+    pthread_exit(0);  
+}
